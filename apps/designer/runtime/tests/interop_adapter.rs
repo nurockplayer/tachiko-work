89dce451be6af0c26bff722397681b9db989cff5
tachikochoko <nurockplayer@gmail.com>
2026-09-20 02:27:26 +0900

    style: format explicit adapter imports

--- apps/designer/runtime/tests/interop_adapter.rs
+++ apps/designer/runtime/tests/interop_adapter.rs
@@ -1,8 +1,8 @@
 use std::io::{Cursor, Read, Write};
 use tachiko_designer_runtime::interop_adapter::{
-    FidelityCategory, ImportOptions, MAX_COLUMNS, MAX_DATA_ROWS, MAX_EXPANDED_BYTES,
-    MAX_FORMULAS, MAX_SHEETS, MAX_SOURCE_BYTES, MAX_ZIP_ENTRIES, SourceValue, SourceWorkbook,
-    export_csv, export_xlsx, import_csv, import_xlsx,
+    FidelityCategory, ImportOptions, MAX_COLUMNS, MAX_DATA_ROWS, MAX_EXPANDED_BYTES, MAX_FORMULAS,
+    MAX_SHEETS, MAX_SOURCE_BYTES, MAX_ZIP_ENTRIES, SourceValue, SourceWorkbook, export_csv,
+    export_xlsx, import_csv, import_xlsx,
 };
 use zip::{ZipArchive, ZipWriter, write::SimpleFileOptions};
 fn simple() -> SourceWorkbook {
